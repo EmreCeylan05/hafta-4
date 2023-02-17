da922a8c5b4caacfecbf2eb7328e268ffe6eed38
EmreCeylan05 <emre05ceylan9@gmail.com>
2023-02-17 22:44:38 +0300

    First commit.

---
+++ script.js
@@ -0,0 +1,22 @@
+var x=9;
+var y=5;
+function topla() {
+    var z= Number(x)+Number(y);
+    alert(z);
+}
+function cikar() {
+    var z= Number(x)-Number(y);
+    alert(z);
+}
+function carp() {
+    var z= Number(x)*Number(y);
+    alert(z);
+}
+function bol() {
+    var z= Number(x)/Number(y);
+    alert(z);
+}
+function mod() {
+    var z= Number(x)%Number(y);
+    alert(z);
+}
